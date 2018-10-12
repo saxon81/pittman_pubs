417e20118e079757a0212ed7cabac998527d72d9
Adam Pittman <adam@wayfache.com>
2018-10-12 16:43:10 -0400

    initial commit

---
+++ app/controllers/sales_controller.rb
@@ -0,0 +1,3 @@
+class SalesController < ApplicationController
+	before_action :authenticate_user!
+end
---
+++ test/controllers/admin_books_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdminBooksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
